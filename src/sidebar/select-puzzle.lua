f80a3e892036f099061bc0edb251d993ff1f6eba
Rebecca Känig <becky@monometric.net>
2021-07-16 20:36:35 +0200

    Add more puzzles

--- src/sidebar/select-puzzle.lua
+++ src/sidebar/select-puzzle.lua
@@ -21,7 +21,7 @@ function SelectPuzzleSidebar:enter(context, selected)
 	for i, id in pairs(creator.created) do
 		local puzzle = Puzzle.load(context, id)
 		local revealed = creator.id == player.id or player.played[puzzle.id]
-		local text = revealed and "\"" .. puzzle.title .. "\"" or "Puzzle " .. i
+		local text = revealed and "\"" .. puzzle.title .. "\"" or string.format("Puzzle %02d", i)
 
 		local image = nil
 		if revealed then
