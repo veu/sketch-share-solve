092ef5077999636fe18e0ba66bd398261ae531f9
Rebecca König <becky@monometric.net>
2023-01-22 11:14:04 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
-rm -rf "Sketch, Share, Solve.pdx/"
-pdc -s ./src "Sketch, Share, Solve.pdx"
 version=$(sed -n -e 's/version=//p' src/pdxinfo)
+rm -rf Sketch,\ Share,\ Solve.pdx/
+rm Sketch,\ Share,\ Solve.$version.pdx.zip Sketch,\ Share,\ Solve.$version-no-music.pdx.zip
+pdc -s ./src Sketch,\ Share,\ Solve.pdx
 zip -r9 Sketch,\ Share,\ Solve.$version.pdx.zip Sketch,\ Share,\ Solve.pdx
 rm Sketch,\ Share,\ Solve.pdx/music/*.pda
 zip -r9 Sketch,\ Share,\ Solve.$version-no-music.pdx.zip Sketch,\ Share,\ Solve.pdx
